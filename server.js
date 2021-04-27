2d655e1b3c39838f68df51884daaed9cdbfb4f8c
srgivp <srgivp@gmail.com>
2021-04-27 20:00:52 +0300

    all routes redirection removed from server

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ const app = express();
 const port = process.env.PORT || 8081;
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 app.get('/', () => {
   const db = new sqlite3.Database('./database/customers.db');
   createPopulateTable(db, 'users', './database/users.json');
